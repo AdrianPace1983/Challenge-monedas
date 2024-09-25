0a9a51db9a004a268fad413b7aafcd4b68dc5cb7
Oswaldo David Rivera Robles <odriverar@hotmail.com>
2024-09-25 07:15:21 -0500

    Configuración y creación de paquetes del proyecto

---
+++ src/drr/aluradesafio/conversormonedas/presentacion/Principal.java
@@ -0,0 +1,7 @@
+package drr.aluradesafio.conversormonedas.presentacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Convertidor de monedas");
+    }
+}
